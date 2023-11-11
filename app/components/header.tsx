6e95d0097d8428dbca1c32b2a20bbc3e20f7cd5c
Supanut1911 <ynotmela@gmail.com>
2023-11-11 16:25:14 +0700

    ✨ feat: set up main page

---
+++ app/components/header.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useEffect } from "react";
+import axios from "axios";
+import Image from "next/image";
+
+const Header = () => {
+  return "header";
+};
+
+export default Header;
