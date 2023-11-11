7257719c7db844094f5a91dd9b8a65d56649dbde
Supanut1911 <ynotmela@gmail.com>
2023-11-11 16:31:23 +0700

    🌈 style(add icon)

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,6 +3,8 @@
 import { useEffect } from "react";
 import axios from "axios";
 import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Logo from "../../public/assets/logo.png";
 
 const Header = () => {
   return "header";
